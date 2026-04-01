d5c33600ed417b0f2cda73b8f2c775ede0310cfb
easyelavazhagi-byte <easyelavazhagi@gmail.com>
2026-04-01 10:11:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+OUTPUT :
+
+Python 3.14.2 (tags/v3.14.2:df79316, Dec 5 2025, 17:18:21) [MSC v.1936 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information
+==================== RESTART: C:/Users/ELAVAZHAGI/Documents/2.py ====================
+
+Enter mark 1: 92
+Enter mark 2: 55
+Enter mark 3: 86
+Total Marks: 233
+Average Marks: 77.66666666666667
+
+>>> 
